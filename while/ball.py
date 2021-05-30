b4fa2e2f89eac6cd752e1b4c2607cf7bfa31d97b
Dayeon Choi <75402494+dayomi02@users.noreply.github.com>
2021-05-30 23:24:29 +0900

    while을 사용하는 반복문

---
+++ while/ball.py
@@ -0,0 +1,7 @@
+height = 100;
+i = 1;
+
+while i<=10:
+    height = height*(3.0/5.0);
+    print(i,round(height,4));
+    i += 1;
---
+++ while/repeat.py
@@ -0,0 +1,6 @@
+print("repeat");
+number = int(input());
+repeat = number;
+while repeat > 0:
+    print(number);
+    repeat -= 1;
